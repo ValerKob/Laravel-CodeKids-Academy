db4ae7b18dbe77e597df0d677783eac56cc5e4db
Val <89907932+ValerKob@users.noreply.github.com>
2025-08-28 03:12:30 +0300

    ама

--- resources/views/includes/main/footer.blade.php
+++ resources/views/includes/main/footer.blade.php
@@ -53,7 +53,7 @@
             href="https://dzen.ru/codekids"
             class="w-8 h-8 bg-gray-800 rounded-full flex items-center justify-center text-gray-300 hover:bg-purple-600 transition duration-300"
             >
-                <img width="20" height="20" style="border-radius: 90%" src="./assets/images/main/links/ya_zen-0000.jpg" alt="">
+                <img width="20" height="20" style="border-radius: 90%" src="https://yastatic.net/naydex/yandex-search/m8nl6cD85/5c82fcRGZ/CWgqL4J-bGToBH7cw_j7VjkJPCemnX43PrkHFt8_fuzpOBRDDHflLLFxAMQ3LzUbWQYGKaFvwN9ZwZcSjuxUlVRlBgarVzl0_EUegJuc3icVcA1m2oV6X0vELwund271yYlJA7yzReh5mexBrvbc6nxCZyiwrnQ" alt="">
             </a>
         </div>
         </div>
